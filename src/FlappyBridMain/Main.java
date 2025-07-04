ebfa0bcb0e5506168dfe38e3dd58766c081f2a92
LucasAraya97 <arayalucas1997@gmail.com>
2025-07-04 00:33:46 -0300

    Agregue Icono Start en el boton.

--- src/FlappyBridMain/Main.java
+++ src/FlappyBridMain/Main.java
@@ -3,6 +3,7 @@ package FlappyBridMain;
 import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
 
 import javax.swing.*;
@@ -23,8 +24,21 @@ public class Main extends JFrame {
         gbc.gridx = 0;
         gbc.gridy = GridBagConstraints.RELATIVE;
         gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.anchor = GridBagConstraints.CENTER; //centrado
+        
+        //imagen del boton start
+        ImageIcon iconoStart = new ImageIcon(getClass().getResource("/media/start.png"));
+        Image img = iconoStart.getImage().getScaledInstance(100, 40, Image.SCALE_SMOOTH);
+        iconoStart = new ImageIcon(img);
 
-        JButton startButton = new JButton("Iniciar Juego");
+        
+        JButton startButton = new JButton(iconoStart);
+        //Opciones para que no sea visible el boton
+        startButton.setBorderPainted(false); //contorno
+        startButton.setContentAreaFilled(false); //no pinte el area
+        startButton.setFocusPainted(false); //resltado de enfoque
+        startButton.setOpaque(false); //el boton no es opaco
+        //accion del boton
         startButton.addActionListener(e -> {
             dispose(); // Cierra el menú
             new RunGameFlappyBird(); // Llama al juego
